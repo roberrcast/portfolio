0b73b9114d1ad23345d984f4facad11e4e8cf71b
robercast <dunamis1.16@gmail.com>
2026-06-17 18:43:39 -0700

    Add theming, fonts(api, otf) material icons

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,3 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle``;
+export const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: 'Mono';
+    src: url('../assets/fonts/abc-favorit-mono-book.otf') format('opentype');
+    font-weight: normal;
+    font-style: normal;
+}
+`;

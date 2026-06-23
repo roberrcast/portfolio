c4cca56b41ded57bd8c16e8ee436471943d5972c
robercast <dunamis1.16@gmail.com>
2026-06-23 16:45:15 -0700

    Add Hero translation.ts en and es objects

---
+++ src/components/Hero/translation.ts
@@ -0,0 +1,15 @@
+export const en = {
+    profileAlt: "Roberto's profile image",
+    subtitle: "Front End Engineer | Digital Craftsman",
+    description:
+        "Aspiring Front End Engineer specialized in building responsive web applications with React, JavaScript, and TypeScript.",
+    viewProject: "View My Projecs",
+};
+
+export const es = {
+    profileAlt: "Imágen de perfil de Roberto",
+    subtitle: "Ingeniero Front End | Artesano Digital",
+    description:
+        "Aspirante a Ingeniero Front End especializado en la creación de aplicaciones web responsivas con React, JavaScript y TypeScript.",
+    viewProject: "Ver Mis Proyectos",
+};

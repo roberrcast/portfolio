dfe306f343cc69228d10d87f50772f300bed87ba
robercast <dunamis1.16@gmail.com>
2026-06-23 04:27:12 -0700

    Add commons.ts file for translation

---
+++ src/i18n/common.ts
@@ -0,0 +1,17 @@
+export const en = {
+    nav: {
+        works: "Works",
+        about: "About",
+        contact: "Contact",
+        stack: "Stack",
+    },
+};
+
+export const es = {
+    nav: {
+        works: "Proyectos",
+        about: "Acerca de",
+        contact: "Contacto",
+        stack: "Stack",
+    },
+};
